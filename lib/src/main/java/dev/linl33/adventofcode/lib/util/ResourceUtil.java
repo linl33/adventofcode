3a556b0e5f943a03b2bb54b62e89dfc801a1d712
linl33 <linlee1997@gmail.com>
2020-11-17 15:06:25 +0800

    Improve error when input is not found

--- lib/src/main/java/dev/linl33/adventofcode/lib/util/ResourceUtil.java
+++ lib/src/main/java/dev/linl33/adventofcode/lib/util/ResourceUtil.java
@@ -1,17 +1,25 @@
 package dev.linl33.adventofcode.lib.util;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
 public class ResourceUtil {
-  public static BufferedReader readResource(Class<?> clazz, String resource) {
+  @NotNull
+  public static BufferedReader readResource(@NotNull Class<?> clazz, @NotNull String resource) {
     Objects.requireNonNull(clazz);
     Objects.requireNonNull(resource);
 
+    var resourceStream = clazz.getResourceAsStream(resource);
+
+    if (resourceStream == null) {
+      throw new UncheckedIOException(new FileNotFoundException(resource));
+    }
+
     return new BufferedReader(new InputStreamReader(
-        clazz.getResourceAsStream(resource),
+        resourceStream,
         StandardCharsets.UTF_8
     ));
   }
