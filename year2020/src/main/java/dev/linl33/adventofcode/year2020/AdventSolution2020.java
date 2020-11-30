3ac23375d79a49bb69d6d16990b8abd907d6e042
linl33 <linlee1997@gmail.com>
2020-11-30 22:06:49 +0800

    Init year2020

---
+++ year2020/src/main/java/dev/linl33/adventofcode/year2020/AdventSolution2020.java
@@ -0,0 +1,10 @@
+package dev.linl33.adventofcode.year2020;
+
+import dev.linl33.adventofcode.lib.solution.AbsAdventSolution;
+
+public abstract class AdventSolution2020<T1, T2> extends AbsAdventSolution<T1, T2> {
+  @Override
+  public int getYear() {
+    return 2020;
+  }
+}
