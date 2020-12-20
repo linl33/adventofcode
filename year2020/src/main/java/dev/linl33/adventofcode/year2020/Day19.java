e9b9035edbb2c2b128329bddd1bbb2a0182a631c
linl33 <linlee1997@gmail.com>
2020-12-20 12:45:37 +0800

    Improve 2020 day19

--- year2020/src/main/java/dev/linl33/adventofcode/year2020/Day19.java
+++ year2020/src/main/java/dev/linl33/adventofcode/year2020/Day19.java
@@ -5,19 +5,18 @@ import dev.linl33.adventofcode.lib.util.AdventUtil;
 import java.io.BufferedReader;
 import java.nio.CharBuffer;
 import java.util.Arrays;
-import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 import java.util.function.IntFunction;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Day19 extends AdventSolution2020<Integer, Integer> {
   private static final String GROUP_START = "(?:";
   private static final String GROUP_END = ")";
   private static final String REGEX_OR = "|";
+  private static final String REGEX_ANY = ".";
   private static final char CHAR_A = 'a';
   private static final char CHAR_B = 'b';
   private static final String STR_A = "a";
@@ -52,7 +51,7 @@ public class Day19 extends AdventSolution2020<Integer, Integer> {
           var regex31 = Pattern.compile("^" + makeRegex(rules[31], resolver));
 
           return line -> {
-            // use a CharBuffer for fast subSequence
+            // use CharBuffer for fast subSequence
             // have to subSequence because Matcher.find(int start) doesn't respect start of line anchor '^'
             var cb = CharBuffer.wrap(line);
 
@@ -88,7 +87,7 @@ public class Day19 extends AdventSolution2020<Integer, Integer> {
   }
 
   private static int countMatches(BufferedReader reader,
-                                  Function<List<String>[], Predicate<String>> rulesToPredicate) {
+                                  Function<int[], Predicate<String>> rulesToPredicate) {
     var input = AdventUtil.readInputGrouped(reader).iterator();
     var rules = parseRules(input.next());
 
@@ -97,69 +96,119 @@ public class Day19 extends AdventSolution2020<Integer, Integer> {
         .count();
   }
 
-  private static List<String>[] parseRules(Stream<String> stream) {
-    // use single-character split to avoid regex compilation in split
-    // keep track of the max because the IDs in example input for part 2 are discontinuous
-
-    @SuppressWarnings("unchecked")
-    var output = (List<String>[]) new List[1 << 8];
-    var aMax = new AtomicInteger(-1);
+  private static int[] parseRules(Stream<String> stream) {
+    var output = new int[1 << 8];
+    var max = new AtomicInteger(-1);
 
     stream.forEach(line -> {
       var sep = line.indexOf(':');
       var id = Integer.parseInt(line, 0, sep, 10);
 
-      var orIdx = line.indexOf('|');
-      if (orIdx < 0) {
-        output[id] = List.of(line.substring(sep + 2));
-      } else {
-        output[id] = List.of(
-            line.substring(sep + 2, orIdx - 1),
-            line.substring(orIdx + 2)
-        );
+      var rule = 0;
+      var mul = 1;
+      var next = 0;
+      for (int i = line.length() - 1; i >= sep + 1; i--) {
+        var c = line.charAt(i);
+
+        if (c == '"') {
+          rule = line.charAt(i - 1);
+          break;
+        }
+
+        switch (c) {
+          case ' ' -> {
+            rule <<= 8;
+            rule |= next;
+            mul = 1;
+            next = 0;
+          }
+          case '|' -> {
+            // handle the case where the right hand side of an or clause has only 1 term
+
+            // make sure that term ends up at the left most position
+            // it has to be the left most position because
+            // the makeRegex method uses the value of the left most 8 bits
+            // to infer if a rule contains an or clause
+            if ((rule & 0xFF00) == 0) {
+              rule <<= 16;
+            }
+
+            i--;
+          }
+          default -> {
+            next += mul * (c - '0');
+            mul *= 10;
+          }
+        }
       }
 
-      if (id > aMax.getPlain()) {
-        aMax.setPlain(id);
+      output[id] = rule;
+      if (id > max.getPlain()) {
+        max.setPlain(id);
       }
     });
 
-    return Arrays.copyOf(output, aMax.getPlain() + 1);
+    return Arrays.copyOf(output, max.getPlain() + 1);
   }
 
-  private static String makeRegex(List<String> rule, IntFunction<String> resolveRule) {
-    if (rule.size() > 1) {
+  private static String makeRegex(int ruleSet, IntFunction<String> resolveRule) {
+    // if the left most 8 bits are set then this is an or clause
+    if ((ruleSet & 0xFF000000) != 0) {
       // or always joins exactly 2 clauses
-      var left = makeRegex(rule.get(0), resolveRule);
-      var right = makeRegex(rule.get(1), resolveRule);
+      var left = makeRegexSingleRule(ruleSet & 0xFFFF, resolveRule);
+      var right = makeRegexSingleRule((ruleSet & 0xFFFF0000) >>> 16, resolveRule);
 
       if (left.charAt(0) == right.charAt(0) && (left.charAt(0) == CHAR_A || left.charAt(0) == CHAR_B)) {
-        return left.charAt(0) + GROUP_START + left.substring(1) + REGEX_OR + right.substring(1) + GROUP_END;
+        if (left.length() == right.length() && left.length() == 2) {
+          // this clause is prefix(?:a|b) or prefix(?:b|a)
+          // which is equivalent to matching prefix + any character
+          return left.charAt(0) + REGEX_ANY;
+        } else {
+          return left.charAt(0) + GROUP_START + left.substring(1) + REGEX_OR + right.substring(1) + GROUP_END;
+        }
       } else {
-        return GROUP_START + left + REGEX_OR + right + GROUP_END;
+        if (left.length() == 1 && right.length() == 1) {
+          // this clause is (?:a|b) or (?:b|a)
+          // which is equivalent to matching any character
+          return REGEX_ANY;
+        } else {
+          return GROUP_START + left + REGEX_OR + right + GROUP_END;
+        }
       }
     }
 
-    return makeRegex(rule.get(0), resolveRule);
+    return makeRegexSingleRule(ruleSet, resolveRule);
   }
 
-  private static String makeRegex(String oneRule, IntFunction<String> resolveRule) {
-    if (oneRule.length() > 1 && (oneRule.charAt(1) == CHAR_A || oneRule.charAt(1) == CHAR_B)) {
-      return oneRule.charAt(1) == CHAR_A ? STR_A : STR_B;
+  private static String makeRegexSingleRule(int oneRule, IntFunction<String> resolveRule) {
+    if (oneRule == CHAR_A || oneRule == CHAR_B) {
+      return oneRule == CHAR_A ? STR_A : STR_B;
     } else {
-      return Arrays
-          .stream(oneRule.split(" "))
-          .mapToInt(Integer::parseInt)
-          .mapToObj(resolveRule)
-          .collect(Collectors.joining());
+      // most rules have 2 terms
+      // the example for part 1 has a rule with 3 terms
+
+      var first = oneRule & 0xFF;
+      var second = (oneRule & 0xFF00) >>> 8;
+      var third = (oneRule & 0xFF0000) >>> 16;
+
+      if (third != 0) {
+        return resolveRule.apply(first) + resolveRule.apply(second) + resolveRule.apply(third);
+      }
+
+      // if exactly one of them is 0
+      if ((first == 0) != (second == 0)) {
+        return resolveRule.apply(first | second);
+      }
+
+      return resolveRule.apply(first) + resolveRule.apply(second);
     }
   }
 
   private static class RuleResolver implements IntFunction<String> {
-    private final List<String>[] rules;
+    private final int[] rules;
     private final String[] cache;
 
-    private RuleResolver(List<String>[] rules) {
+    private RuleResolver(int[] rules) {
       this.rules = rules;
       this.cache = new String[rules.length];
     }
