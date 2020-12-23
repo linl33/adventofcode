34a1cf95b6e7a56496ea1325013cce8501c3b020
linl33 <linlee1997@gmail.com>
2020-12-23 22:24:01 +0800

    Improve 2020 day23

--- year2020/src/main/java/dev/linl33/adventofcode/year2020/Day23.java
+++ year2020/src/main/java/dev/linl33/adventofcode/year2020/Day23.java
@@ -13,12 +13,30 @@ public class Day23 extends AdventSolution2020<Integer, Long> {
 
   @Override
   public Integer part1(BufferedReader reader) throws IOException {
+    var list = solveArray(reader, 9, 100);
+
+    var sum = 0;
+    var curr = 1;
+
+    while ((curr = list[curr]) != 1) {
+      sum = sum * 10 + curr;
+    }
+
+    return sum;
+  }
+
+  public int part1ByLinkedList(BufferedReader reader) throws IOException {
     var table = simulate(reader, 9, 100);
     return Integer.parseInt(table[1].toString().replace(" ", "").substring(1));
   }
 
   @Override
   public Long part2(BufferedReader reader) throws IOException {
+    var list = solveArray(reader, 1_000_000, 10_000_000);
+    return ((long) list[1]) * ((long) list[list[1]]);
+  }
+
+  public long part2ByLinkedList(BufferedReader reader) throws IOException {
     var table = simulate(reader, 1_000_000, 10_000_000);
 
     var next = table[1].getNext();
@@ -119,5 +137,57 @@ public class Day23 extends AdventSolution2020<Integer, Long> {
     var next2 = toInsert.getNext().getNext();
     tmp.setPrev(next2);
     next2.setNext(tmp);
+  }
+
+  private static int[] solveArray(BufferedReader reader, int cups, int rounds) throws IOException {
+    // this implementation uses an int[] like a linked list
+    // 34251 is translated to [0, 3, 5, 4, 2, 1]
+
+    var seed = Arrays
+        .stream(reader.readLine().split(""))
+        .mapToInt(Integer::parseInt)
+        .toArray();
+
+    var list = new int[cups + 1];
+    var curr = seed[0];
+    for (int i = 1; i < seed.length; i++) {
+      list[curr] = (curr = seed[i]);
+    }
+
+    for (int i = seed.length + 1; i <= cups; i++) {
+      list[curr] = (curr = i);
+    }
+
+    // link the last node to the start
+    list[curr] = (curr = seed[0]);
+
+    for (int n = 0; n < rounds; n++) {
+      var next = list[curr];
+      var next2 = list[list[list[curr]]];
+
+      var dest = curr - 1;
+      while (dest < 1 || (dest == next || dest == next2 || dest == list[list[curr]])) {
+        if (dest < 1) {
+          dest = cups;
+        } else {
+          dest--;
+        }
+      }
+
+      // unlink the 3 numbers
+      // link curr.next to next2.next
+      list[curr] = list[next2];
+
+      // link the 3 numbers to dest
+      // link next2.next to dest.next
+      list[next2] = list[dest];
+      // link dest.next to next;
+      list[dest] = next;
+
+      // advance curr
+      curr = list[curr];
+    }
+
+    return list;
   }
 }
--- year2020/src/test/java/dev/linl33/adventofcode/year2020/test/Day23Test.java
+++ year2020/src/test/java/dev/linl33/adventofcode/year2020/test/Day23Test.java
@@ -1,10 +1,17 @@
 package dev.linl33.adventofcode.year2020.test;
 
+import dev.linl33.adventofcode.lib.function.ThrowingBiFunction;
 import dev.linl33.adventofcode.lib.solution.AdventSolution;
+import dev.linl33.adventofcode.lib.solution.ResourceIdentifier;
 import dev.linl33.adventofcode.testlib.AdventSolutionTest;
+import dev.linl33.adventofcode.testlib.Part1Source;
 import dev.linl33.adventofcode.year2020.Day23;
+import org.junit.jupiter.params.ParameterizedTest;
 
+import java.io.BufferedReader;
 import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Day23Test implements AdventSolutionTest<Integer, Long> {
   @Override
@@ -26,5 +33,11 @@ class Day23Test implements AdventSolutionTest<Integer, Long> {
         newSolutionInstance().getPart1Resource(), 111057672960L,
         "day23test1", 149245887792L
     );
+  }
+
+  @ParameterizedTest
+  @Part1Source
+  void testPart1ByLinkedList(ResourceIdentifier resource, int expected, Day23 instance) {
+    assertEquals(expected, instance.run((ThrowingBiFunction<Day23, BufferedReader, Integer>) Day23::part1ByLinkedList, resource));
   }
 }
