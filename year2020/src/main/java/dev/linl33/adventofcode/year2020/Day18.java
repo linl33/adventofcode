793e73dac0afc23465a06889b2dba98357e38377
linl33 <linlee1997@gmail.com>
2020-12-27 13:19:38 +0800

    Implement 2020 day18 with bytebuffer

--- year2020/src/main/java/dev/linl33/adventofcode/year2020/Day18.java
+++ year2020/src/main/java/dev/linl33/adventofcode/year2020/Day18.java
@@ -1,12 +1,16 @@
 package dev.linl33.adventofcode.year2020;
 
-import java.io.BufferedReader;
+import dev.linl33.adventofcode.lib.solution.ByteBufferAdventSolution;
+import dev.linl33.adventofcode.lib.solution.NullBufferedReaderSolution;
+import dev.linl33.adventofcode.lib.solution.ResourceIdentifier;
 
-public class Day18 extends AdventSolution2020<Long, Long> {
-  private static final char ADD = '+';
-  private static final char MUL = '*';
-  private static final char PAREN_START = '(';
-  private static final char PAREN_END = ')';
+import java.nio.ByteBuffer;
+
+public class Day18 extends AdventSolution2020<Long, Long> implements ByteBufferAdventSolution<Long, Long>, NullBufferedReaderSolution<Long, Long> {
+  private static final byte ADD = '+';
+  private static final byte MUL = '*';
+  private static final byte PAREN_START = '(';
+  private static final byte PAREN_END = ')';
   private static final int ALLOC_SIZE = 1 << 7;
   private static final long ZERO = '0';
 
@@ -15,61 +19,68 @@ public class Day18 extends AdventSolution2020<Long, Long> {
   }
 
   @Override
-  public Long part1(BufferedReader reader) {
-    var combinedDeque = new long[ALLOC_SIZE];
-
-    return reader
-        .lines()
-        .mapToLong(exp -> eval(exp, false, combinedDeque))
-        .sum();
+  public Long part1(ResourceIdentifier identifier) throws Exception {
+    return ByteBufferAdventSolution.super.part1(identifier);
   }
 
   @Override
-  public Long part2(BufferedReader reader) {
-    var combinedDeque = new long[ALLOC_SIZE];
-
-    return reader
-        .lines()
-        .mapToLong(exp -> eval(exp, true, combinedDeque))
-        .sum();
+  public Long part2(ResourceIdentifier identifier) throws Exception {
+    return ByteBufferAdventSolution.super.part2(identifier);
   }
 
-  private static long eval(String exp, boolean opPrecedence, long[] combinedDeque) {
-    // fill output from the end, so ops can be copied with arraycopy
-    // and they can share 1 array
-    var outHead = combinedDeque.length;
-    var opStackHead = -1;
+  @Override
+  public Long part1(ByteBuffer byteBuffer) throws Exception {
+    return eval(byteBuffer, false);
+  }
 
-    // converts the expression to rpn using shunting-yard
-    // https://en.wikipedia.org/wiki/Shunting-yard_algorithm
+  @Override
+  public Long part2(ByteBuffer byteBuffer) throws Exception {
+    return eval(byteBuffer, true);
+  }
 
-    for (char element : exp.toCharArray()) {
-      switch (element) {
-        case ' ' -> {}
-        case PAREN_START -> combinedDeque[++opStackHead] = element;
-        case PAREN_END -> {
-          while (combinedDeque[opStackHead] != PAREN_START) {
-            combinedDeque[--outHead] = combinedDeque[opStackHead--];
+  private static long eval(ByteBuffer byteBuffer, boolean opPrecedence) {
+    var combinedDeque = new long[ALLOC_SIZE];
+    var sum = 0L;
+
+    while (byteBuffer.hasRemaining()) {
+      // fill output from the end, so ops can be copied with arraycopy
+      // and they can share 1 array
+      var outHead = combinedDeque.length;
+      var opStackHead = -1;
+
+      // converts the expression to rpn using shunting-yard
+      // https://en.wikipedia.org/wiki/Shunting-yard_algorithm
+
+      byte element;
+      while ((element = byteBuffer.get()) != '\n') {
+        switch (element) {
+          case ' ' -> {}
+          case PAREN_START -> combinedDeque[++opStackHead] = element;
+          case PAREN_END -> {
+            while (combinedDeque[opStackHead] != PAREN_START) {
+              combinedDeque[--outHead] = combinedDeque[opStackHead--];
+            }
+
+            opStackHead--;
           }
+          case ADD, MUL -> {
+            while (opStackHead >= 0 &&
+                (!opPrecedence || combinedDeque[opStackHead] == ADD) &&
+                combinedDeque[opStackHead] != PAREN_START) {
+              combinedDeque[--outHead] = combinedDeque[opStackHead--];
+            }
 
-          opStackHead--;
-        }
-        case ADD, MUL -> {
-          while (opStackHead >= 0 &&
-              (!opPrecedence || combinedDeque[opStackHead] == ADD) &&
-              combinedDeque[opStackHead] != PAREN_START) {
-            combinedDeque[--outHead] = combinedDeque[opStackHead--];
+            combinedDeque[++opStackHead] = element;
           }
-
-          combinedDeque[++opStackHead] = element;
+          default -> combinedDeque[--outHead] = element;
         }
-        default -> combinedDeque[--outHead] = element;
       }
+
+      System.arraycopy(combinedDeque, 0, combinedDeque, outHead - (opStackHead + 1), opStackHead + 1);
+      sum += evalRpn(combinedDeque, outHead - (opStackHead + 1));
     }
 
-    System.arraycopy(combinedDeque, 0, combinedDeque, outHead - (opStackHead + 1), opStackHead + 1);
-
-    return evalRpn(combinedDeque, outHead - (opStackHead + 1));
+    return sum;
   }
 
   private static long evalRpn(long[] rpn, int size) {
