dbd8106170ceb681d1f9a3eef477265d8a299875
linl33 <linlee1997@gmail.com>
2021-12-02 14:51:06 +0800

    Initialize year2021

---
+++ year2021/src/test/java/module-info.java
@@ -0,0 +1,9 @@
+open module dev.linl33.adventofcode.year2021.test {
+  requires dev.linl33.adventofcode.lib;
+  requires dev.linl33.adventofcode.testlib;
+  requires dev.linl33.adventofcode.year2021;
+
+  requires org.junit.jupiter.api;
+  requires org.junit.jupiter.engine;
+  requires org.junit.jupiter.params;
+}
