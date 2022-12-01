bd2846783ad4caf079993c97100ae8bbe6a7f81a
linl33 <linlee1997@gmail.com>
2022-12-01 12:41:46 +0800

    Init year2022

---
+++ year2022/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module dev.linl33.adventofcode.year2022 {
+  requires transitive dev.linl33.adventofcode.lib;
+  requires dev.linl33.adventofcode.jmh;
+
+  requires org.apache.logging.log4j;
+  requires static org.apache.logging.log4j.core;
+  requires static org.jetbrains.annotations;
+
+//  TODO:
+//  exports dev.linl33.adventofcode.year2022 to dev.linl33.adventofcode.year2022.test;
+
+  opens dev.linl33.adventofcode.year2022 to dev.linl33.adventofcode.lib, dev.linl33.adventofcode.jmh;
+}
