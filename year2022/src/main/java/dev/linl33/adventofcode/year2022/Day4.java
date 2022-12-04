87b31e6dc4d0f665b2a30afff6ae1b688624aab6
linl33 <linlee1997@gmail.com>
2022-12-04 18:44:01 +0800

    Add year 2022 day4

---
+++ year2022/src/main/java/dev/linl33/adventofcode/year2022/Day4.java
@@ -0,0 +1,80 @@
+package dev.linl33.adventofcode.year2022;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.BufferedReader;
+
+public class Day4 extends AdventSolution2022<Integer, Integer> {
+  public static void main(String[] args) {
+    new Day4().runAndPrintAll();
+  }
+
+  @Override
+  public Integer part1(@NotNull BufferedReader reader) {
+    return (int) reader
+        .lines()
+        .filter(line -> {
+          var leftDashPos = line.indexOf('-');
+          var commaPos = line.indexOf(',', leftDashPos + 1);
+          var rightDashPos = line.indexOf('-', commaPos + 1);
+
+          var leftLo = Integer.parseInt(line, 0, leftDashPos, 10);
+          var rightLo = Integer.parseInt(line, commaPos + 1, rightDashPos, 10);
+
+          if (leftLo == rightLo) {
+            return true;
+          }
+
+          var leftHi = Integer.parseInt(line, leftDashPos + 1, commaPos, 10);
+          var rightHi = Integer.parseInt(line, rightDashPos + 1, line.length(), 10);
+
+          if (leftHi == rightHi) {
+            return true;
+          }
+
+          if (leftLo > rightLo) {
+            rightLo = leftLo;
+
+            leftHi ^= rightHi;
+            rightHi ^= leftHi;
+            leftHi ^= rightHi;
+          }
+
+          return rightLo <= leftHi && rightHi <= leftHi;
+        })
+        .count();
+  }
+
+  @Override
+  public Integer part2(@NotNull BufferedReader reader) {
+    return (int) reader
+        .lines()
+        .filter(line -> {
+          var leftDashPos = line.indexOf('-');
+          var commaPos = line.indexOf(',', leftDashPos + 1);
+          var rightDashPos = line.indexOf('-', commaPos + 1);
+
+          var leftLo = Integer.parseInt(line, 0, leftDashPos, 10);
+          var rightLo = Integer.parseInt(line, commaPos + 1, rightDashPos, 10);
+
+          if (leftLo == rightLo) {
+            return true;
+          }
+
+          var leftHi = Integer.parseInt(line, leftDashPos + 1, commaPos, 10);
+          var rightHi = Integer.parseInt(line, rightDashPos + 1, line.length(), 10);
+
+          if (leftHi == rightHi) {
+            return true;
+          }
+
+          if (leftLo > rightLo) {
+            rightLo = leftLo;
+            leftHi = rightHi;
+          }
+
+          return rightLo <= leftHi;
+        })
+        .count();
+  }
+}
--- year2022/src/test/java/dev/linl33/adventofcode/year2022/test/Day4Test.java
+++ year2022/src/test/java/dev/linl33/adventofcode/year2022/test/Day4Test.java
@@ -2,29 +2,29 @@ package dev.linl33.adventofcode.year2022.test;
 
 import dev.linl33.adventofcode.lib.solution.AdventSolution;
 import dev.linl33.adventofcode.testlib.AdventSolutionTest;
-import dev.linl33.adventofcode.year2022.Day3;
+import dev.linl33.adventofcode.year2022.Day4;
 
 import java.util.Map;
 
-class Day3Test implements AdventSolutionTest<Integer, Integer> {
+class Day4Test implements AdventSolutionTest<Integer, Integer> {
   @Override
   public AdventSolution<Integer, Integer> newSolutionInstance() {
-    return new Day3();
+    return new Day4();
   }
 
   @Override
   public Map<Object, Integer> getPart1Cases() {
     return Map.of(
-        newSolutionInstance().getPart1Resource(), 8072,
-        "day3test1", 157
+        newSolutionInstance().getPart1Resource(), 550,
+        "day4test1", 2
     );
   }
 
   @Override
   public Map<Object, Integer> getPart2Cases() {
     return Map.of(
-        newSolutionInstance().getPart2Resource(), 2567,
-        "day3test1", 70
+        newSolutionInstance().getPart2Resource(), 931,
+        "day4test1", 4
     );
   }
 }
