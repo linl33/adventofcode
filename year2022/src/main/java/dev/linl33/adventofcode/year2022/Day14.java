6652e448027bcb9b659bac8eccb079774080a9ae
linl33 <linlee1997@gmail.com>
2022-12-14 23:19:49 +0800

    Improve year 2022 day14

--- year2022/src/main/java/dev/linl33/adventofcode/year2022/Day14.java
+++ year2022/src/main/java/dev/linl33/adventofcode/year2022/Day14.java
@@ -1,17 +1,20 @@
 package dev.linl33.adventofcode.year2022;
 
-import dev.linl33.adventofcode.lib.grid.ColumnArrayGrid;
 import dev.linl33.adventofcode.lib.grid.Grid;
 import dev.linl33.adventofcode.lib.grid.RowArrayGrid;
-import dev.linl33.adventofcode.lib.point.Point2D;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.BufferedReader;
-import java.util.Arrays;
+import java.util.regex.Pattern;
 
 public class Day14 extends AdventSolution2022<Integer, Integer> {
   private static final int MAX_HEIGHT = 200;
-  private static final int MAX_WIDTH = MAX_HEIGHT + 500;
+  private static final int SAND_SOURCE_X = 100;
+  private static final int SAND_SOURCE_X_DELTA = SAND_SOURCE_X - 500;
+
+  private static final int MAX_WIDTH = 1 << (Integer.SIZE - Integer.numberOfLeadingZeros(SAND_SOURCE_X * 2 + 1 - 1));
+
+  private static final Pattern ARROW_REGEX = Pattern.compile(" -> ");
 
   public static void main(String[] args) {
     new Day14().runAndPrintAll();
@@ -19,55 +22,137 @@ public class Day14 extends AdventSolution2022<Integer, Integer> {
 
   @Override
   public Integer part1(@NotNull BufferedReader reader) throws Exception {
-    var grid = new ColumnArrayGrid(MAX_HEIGHT, MAX_WIDTH);
+    var grid = new RowArrayGrid(MAX_HEIGHT, MAX_WIDTH);
+    var gridArray = grid.array();
     var floor = buildGrid(reader, grid);
 
-    for (int x = 0; x < grid.width(); x++) {
-      grid.set(x, floor + 1, '#');
-    }
+    var stack = new int[MAX_HEIGHT];
+    var stackPointer = 0;
+    stack[0] = SAND_SOURCE_X;
 
     var sand = 0;
-    Point2D sandPos;
+    while (stackPointer >= 0) {
+      var currIdx = stack[stackPointer];
 
-    do {
-      sandPos = addSand(grid);
+      if (currIdx > floor * grid.width()) {
+        return sand;
+      }
+
+      var nextIdx = currIdx + grid.width();
+      if (gridArray[nextIdx] == 0) {
+        stack[++stackPointer] = nextIdx;
+        continue;
+      }
+
+      nextIdx = nextIdx - 1;
+      if (gridArray[nextIdx] == 0) {
+        stack[++stackPointer] = nextIdx;
+        continue;
+      }
+
+      nextIdx = nextIdx + 2;
+      if (gridArray[nextIdx] == 0) {
+        stack[++stackPointer] = nextIdx;
+        continue;
+      }
+
+      stackPointer--;
+      gridArray[currIdx] = 'o';
       sand++;
-    } while (sandPos.y() < floor);
+    }
 
-    return sand - 1;
+    return sand;
   }
 
   @Override
   public Integer part2(@NotNull BufferedReader reader) throws Exception {
     var grid = new RowArrayGrid(MAX_HEIGHT, MAX_WIDTH);
+    var gridArray = grid.array();
     var floor = buildGrid(reader, grid);
 
-    for (int x = 0; x < grid.width(); x++) {
-      grid.set(x, floor + 2, '#');
+    var minX = SAND_SOURCE_X;
+    var maxX = SAND_SOURCE_X;
+
+    for (int y = floor, offset = floor * grid.width(); y > 0; y--, offset -= grid.width()) {
+      for (int x = SAND_SOURCE_X - y + 1, p = offset + x; x < minX; x++, p++) {
+        if (gridArray[p] != 0) {
+          minX = x;
+          break;
+        }
+      }
+
+      for (int x = SAND_SOURCE_X + y - 1, p = offset + x; x > maxX; x--, p--) {
+        if (gridArray[p] != 0) {
+          maxX = x;
+          break;
+        }
+      }
     }
 
+    var stack = new int[MAX_HEIGHT];
+    var stackPointer = 0;
+    stack[0] = SAND_SOURCE_X;
+
     var sand = 0;
-    Point2D sandPos;
+    while (stackPointer >= 0) {
+      var currIdx = stack[stackPointer];
 
-    do {
-      sandPos = addSand(grid);
+      if (currIdx < (floor + 1) * grid.width()) {
+        var nextIdx = currIdx + grid.width();
+        if (gridArray[nextIdx] == 0) {
+          stack[++stackPointer] = nextIdx;
+          continue;
+        }
+
+        var x = currIdx % grid.width();
+        nextIdx = nextIdx - 1;
+        if (x >= minX && gridArray[nextIdx] == 0) {
+          stack[++stackPointer] = nextIdx;
+          continue;
+        }
+
+        nextIdx = nextIdx + 2;
+        if (x <= maxX && gridArray[nextIdx] == 0) {
+          stack[++stackPointer] = nextIdx;
+          continue;
+        }
+      }
+
+      stackPointer--;
+      gridArray[currIdx] = 'o';
       sand++;
-    } while (sandPos.y() > 0);
 
-    return sand;
+      if (currIdx < grid.width()) {
+        break;
+      }
+    }
+
+    var sandMin = SAND_SOURCE_X - (floor + 1);
+    var sandMax = SAND_SOURCE_X + (floor + 1);
+
+    var leftWidth = (minX - 2) - sandMin + 1;
+    var rightWidth = sandMax - (maxX + 2) + 1;
+
+    var leftArea = leftWidth * (leftWidth + 1) / 2;
+    var rightArea = rightWidth * (rightWidth + 1) / 2;
+
+    return sand + leftArea + rightArea;
   }
 
   private static int buildGrid(@NotNull BufferedReader reader, Grid grid) {
-    var input = reader.lines().toList();
+    var input = reader.lines().toArray(String[]::new);
 
-    var floor = -1;
+    var floor = 0;
+    var left = new int[2];
+    var right = new int[2];
 
-    for (String line : input) {
-      var endpoints = line.split(" -> ");
+    for (var line : input) {
+      var endpoints = ARROW_REGEX.split(line);
+      parsePair(endpoints[0], left);
+      var next = right;
 
-      for (int i = 0; i < endpoints.length - 1; i++) {
-        var left = Arrays.stream(endpoints[i].split(",")).mapToInt(Integer::valueOf).toArray();
-        var right = Arrays.stream(endpoints[i + 1].split(",")).mapToInt(Integer::valueOf).toArray();
+      for (int i = 1; i < endpoints.length; i++) {
+        parsePair(endpoints[i], next);
 
         if (left[0] == right[0]) {
           var x = left[0];
@@ -85,43 +170,21 @@ public class Day14 extends AdventSolution2022<Integer, Integer> {
           for (int x = xMin; x <= xMax; x++) {
             grid.set(x, y, '#');
           }
+
+          floor = Math.max(floor, y);
         } else {
           throw new IllegalArgumentException();
         }
 
-        floor = Math.max(floor, left[1]);
-        floor = Math.max(floor, right[1]);
+        next = next == left ? right : left;
       }
     }
 
     return floor;
   }
 
-  private static Point2D addSand(Grid grid) {
-    var next = new Point2D(500, 0);
-    var curr = next;
-
-    do {
-      curr = next;
-      var below = curr.translate(0, 1);
-      var left = curr.translate(-1, 1);
-      var right = curr.translate(1, 1);
-
-      next = below;
-      if (grid.get(next) != 0) {
-        next = left;
-
-        if (grid.get(next) != 0) {
-          next = right;
-
-          if (grid.get(next) != 0) {
-            next = null;
-          }
-        }
-      }
-    } while (next != null);
-
-    grid.set(curr.x(), curr.y(), 'o');
-    return curr;
+  private static void parsePair(String pairStr, int[] pair) {
+    pair[0] = Integer.parseInt(pairStr, 0, 3, 10) + SAND_SOURCE_X_DELTA;
+    pair[1] = Integer.parseInt(pairStr, 4, pairStr.length(), 10);
   }
 }
