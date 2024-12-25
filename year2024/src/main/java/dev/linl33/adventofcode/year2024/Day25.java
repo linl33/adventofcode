d329a25fbbe384f1990991a7c059f2188e7b2ade
linl33 <linlee1997@gmail.com>
2024-12-25 13:29:57 +0800

    Improve year 2024 day25

--- year2024/src/main/java/dev/linl33/adventofcode/year2024/Day25.java
+++ year2024/src/main/java/dev/linl33/adventofcode/year2024/Day25.java
@@ -1,16 +1,12 @@
 package dev.linl33.adventofcode.year2024;
 
-import dev.linl33.adventofcode.lib.util.AdventUtil;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.BufferedReader;
-import java.util.ArrayList;
-import java.util.stream.Collectors;
 
 public class Day25 extends AdventSolution2024<Integer, Void> {
   private static final int WIDTH = 5;
   private static final int HEIGHT = 7;
-  private static final String FULL_ROW = "#".repeat(WIDTH);
 
   public static void main() {
     new Day25().runAndPrintAll();
@@ -18,43 +14,36 @@ public class Day25 extends AdventSolution2024<Integer, Void> {
 
   @Override
   public Integer part1(@NotNull BufferedReader reader) {
-    var groups = AdventUtil.readInputGrouped(reader).map(x -> x.collect(Collectors.joining())).toArray(String[]::new);
+    // TODO: SIMD should help a lot here
 
-    var keys = new ArrayList<int[]>();
-    var locks = new ArrayList<int[]>();
+    var lines = reader.lines().toArray(String[]::new);
 
-    for (var i = 0; i < groups.length; i++) {
-      var g = groups[i];
+    var locksAndKeys = new int[(lines.length + 1) / (HEIGHT + 1)];
+    var lockIdx = 0;
+    var keyIdx = locksAndKeys.length - 1;
 
-      var shape = new int[WIDTH];
-      for (var row = 0; row < HEIGHT; row++) {
+    for (var i = 0; i < lines.length; i += HEIGHT + 1) {
+      var shape = 0;
+      for (var row = 1; row < HEIGHT - 1; row++) {
         for (var col = 0; col < WIDTH; col++) {
-          if (g.charAt(row * WIDTH + col) == '#') {
-            shape[col] += 1;
-          }
+          // '#' & 1 == 1, '.' & 1 == 0
+          var bit = lines[i + row].codePointAt(col) & 1;
+          shape |= bit << (row * WIDTH + col);
         }
       }
 
-      (g.startsWith(FULL_ROW) ? locks : keys).add(shape);
+      locksAndKeys[lines[i].codePointAt(0) == '#' ? lockIdx++ : keyIdx--] = shape;
     }
 
     var count = 0;
-    for (var i = 0; i < locks.size(); i++) {
-      var lock = locks.get(i);
+    for (int i = 0; i < lockIdx; i++) {
+      var lock = locksAndKeys[i];
 
-      for (var j = 0; j < keys.size(); j++) {
-        var key = keys.get(j);
+      for (int j = lockIdx; j < locksAndKeys.length; j++) {
+        var key = locksAndKeys[j];
 
-        var fits = true;
-        for (var k = 0; k < WIDTH; k++) {
-          if (lock[k] + key[k] > HEIGHT) {
-            fits = false;
-            break;
-          }
-        }
-
-        if (fits) {
-          count += 1;
+        if ((key & lock) == 0) {
+          count++;
         }
       }
     }
